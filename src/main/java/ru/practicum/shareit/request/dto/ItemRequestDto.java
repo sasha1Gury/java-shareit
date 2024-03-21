c91ea7e91f2e8dbbe44eab4736e25dd5626c87c8
sasha1Gury <sashagury456123@mail.ru>
2024-03-21 14:52:57 +0300

    feat: add dto for ItemRequest

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.request.dto;
 
+import lombok.Data;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint add-item-requests.
  */
+@Data
 public class ItemRequestDto {
+    private long id;
+    private String description;
+    private User owner;
+    private LocalDateTime created;
 }
