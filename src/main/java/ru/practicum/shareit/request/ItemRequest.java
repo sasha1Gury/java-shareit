0dc9ac5705cc26b36470ad00e2e3d3cbe3bc6879
sasha1Gury <sashagury456123@mail.ru>
2024-03-21 14:51:30 +0300

    feat: add model for ItemRequest

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.request;
 
+import lombok.Data;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint add-item-requests.
  */
+@Data
 public class ItemRequest {
+    private long id;
+    private String description;
+    private User owner;
+    private LocalDateTime created;
 }
