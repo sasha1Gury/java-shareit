fb2366ed4dd3301110f5a1bf1d63a13d7283e9dc
sasha1Gury <sashagury456123@mail.ru>
2024-03-21 14:51:47 +0300

    feat: add model for user

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,13 @@
 package ru.practicum.shareit.user;
 
+import lombok.Data;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class User {
+    private long id;
+    private String name;
+    private String email;
 }
