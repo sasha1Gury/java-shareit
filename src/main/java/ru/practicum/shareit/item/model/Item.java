a046991e5421d814b3b0e91f614be7e5952f7ccd
sasha1Gury <sashagury456123@mail.ru>
2024-03-21 14:51:17 +0300

    feat: add model for item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.Data;
+import ru.practicum.shareit.user.User;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class Item {
+    private long id;
+    private String name;
+    private String description;
+    private boolean available;
+    private User owner;
+    private String request;
 }
