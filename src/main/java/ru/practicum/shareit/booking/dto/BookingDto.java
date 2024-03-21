f1c73637a13d6509a0f22182c2abdbb3b5b360a5
sasha1Gury <sashagury456123@mail.ru>
2024-03-21 14:52:35 +0300

    feat: add dto for booking

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,21 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.Data;
+import ru.practicum.shareit.booking.Status;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint add-bookings.
  */
+@Data
 public class BookingDto {
+    private long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Item item;
+    private User booker;
+    private Status status;
 }
