1dbdbdc1756dc7a730baeb4f320aad362cf48810
sasha1Gury <sashagury456123@mail.ru>
2024-03-22 22:49:03 +0300

    feat: add NoArgsConstructor

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -2,12 +2,14 @@ package ru.practicum.shareit.user.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * TODO Sprint add-controllers.
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private long id;
     private String name;
