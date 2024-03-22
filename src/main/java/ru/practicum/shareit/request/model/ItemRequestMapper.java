474fc202217f8f813fd30019d98e55583e8d4c8e
sasha1Gury <sashagury456123@mail.ru>
2024-03-22 18:22:00 +0300

    feat: add ItemRequestMapper

---
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequestMapper.java
@@ -0,0 +1,19 @@
+package ru.practicum.shareit.request.model;
+
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+
+public class ItemRequestMapper {
+    public static ItemRequest toEntity(ItemRequestDto dto) {
+        return new ItemRequest(dto.getId(),
+                dto.getDescription(),
+                dto.getOwner(),
+                dto.getCreated());
+    }
+
+    public static ItemRequestDto toDto(ItemRequest itemRequest) {
+        return new ItemRequestDto(itemRequest.getId(),
+                itemRequest.getDescription(),
+                itemRequest.getOwner(),
+                itemRequest.getCreated());
+    }
+}
