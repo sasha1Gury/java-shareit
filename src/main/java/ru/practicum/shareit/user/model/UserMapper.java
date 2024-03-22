6393aa3070c63a7ab1c4d008632adceefbc7d7ac
sasha1Gury <sashagury456123@mail.ru>
2024-03-22 18:22:13 +0300

    feat: add UserMapper

---
+++ src/main/java/ru/practicum/shareit/user/model/UserMapper.java
@@ -0,0 +1,17 @@
+package ru.practicum.shareit.user.model;
+
+import ru.practicum.shareit.user.dto.UserDto;
+
+public class UserMapper {
+    public static User toEntity(UserDto dto) {
+        return new User(dto.getId(),
+                dto.getName(),
+                dto.getEmail());
+    }
+
+    public static UserDto toDto(User user) {
+        return new UserDto(user.getId(),
+                user.getName(),
+                user.getEmail());
+    }
+}
