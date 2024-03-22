5bd7fb88c07c6c92bea7f091b8aa221cfb3588f3
sasha1Gury <sashagury456123@mail.ru>
2024-03-22 17:59:44 +0300

    feat: add allArgsConstructor in BookingDto

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.booking.model.Status;
 import ru.practicum.shareit.item.model.Item;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
  * TODO Sprint add-bookings.
  */
 @Data
+@AllArgsConstructor
 public class BookingDto {
     private long id;
     private LocalDateTime start;
