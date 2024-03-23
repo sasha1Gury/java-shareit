e33000b9ef9cf4bf901d225502fa16c2f9326693
sasha1Gury <sashagury456123@mail.ru>
2024-03-23 17:33:36 +0300

    feat: add ItemServiceImpl

---
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -0,0 +1,10 @@
+package ru.practicum.shareit.item.service;
+
+import ru.practicum.shareit.item.model.Item;
+
+public class ItemServiceImpl implements ItemService {
+    @Override
+    public Item createItem(Item item) {
+
+    }
+}
