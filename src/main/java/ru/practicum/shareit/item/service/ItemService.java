d7059c6692ab38ba89ec22c124fc933dcdf93623
sasha1Gury <sashagury456123@mail.ru>
2024-03-24 01:10:54 +0300

    feat: add updateItem

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -2,6 +2,10 @@ package ru.practicum.shareit.item.service;
 
 import ru.practicum.shareit.item.model.Item;
 
+import java.util.Map;
+
 public interface ItemService {
-    Item createItem(Item item);
+    Item createItem(Item item, long userId);
+
+    Item updateItem(Map<String, Object> updates, long itemId, long userId);
 }
