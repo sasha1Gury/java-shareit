d6dab9ec4e76a271deeb1823f12309157004eac6
sasha1Gury <sashagury456123@mail.ru>
2024-03-24 01:09:52 +0300

    style: add handle new exception

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.shareit.item.exception.AvailableNotInitInItem;
+import ru.practicum.shareit.item.exception.OwnerException;
 import ru.practicum.shareit.user.exception.EmailExistException;
 import ru.practicum.shareit.user.exception.NewUserException;
 
@@ -24,6 +26,18 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleEmailExistException(final EmailExistException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleAvailableNotInitInItem(final AvailableNotInitInItem e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponse handleOwnerException(final OwnerException e) {
         return new ErrorResponse(e.getMessage());
     }
 }
