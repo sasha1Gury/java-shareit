8d445e0cc597be2a2aa8abaa1a8351b3b3f8610d
sasha1Gury <sashagury456123@mail.ru>
2024-03-23 17:08:47 +0300

    fix: add new methods update get delete

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,14 +1,13 @@
 package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.service.UserService;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
 
 /**
  * TODO Sprint add-controllers.
@@ -18,9 +17,30 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    //private final ObjectMapper objectMapper;
 
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
         return userService.createUser(user);
+    }
+
+    @PatchMapping("/{userId}")
+    public User updateUser(@Valid @RequestBody Map<String, Object> updates, @PathVariable("userId") long userId) {
+        return userService.updateUser(updates, userId);
+    }
+
+    @GetMapping("/{userId}")
+    public User findUserById(@PathVariable("userId") long userId) {
+        return userService.findUserById(userId);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable("userId") long userId) {
+        userService.deleteUser(userId);
     }
 }
--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -12,5 +15,21 @@ public class UserService {
 
     public User createUser(User user) {
         return userRepository.createUser(user);
+    }
+
+    public User updateUser(Map<String, Object> updates, long userId) {
+        return userRepository.updateUser(updates, userId);
+    }
+
+    public User findUserById(long userId) {
+        return userRepository.findUserById(userId);
+    }
+
+    public void deleteUser(long userId) {
+        userRepository.deleteUser(userId);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.getAllUsers();
     }
 }
