69768e297d8c4bfa4edfbec1358a0c0d27d4ed08
sasha1Gury <sashagury456123@mail.ru>
2024-04-09 19:24:50 +0300

    feat: add addBookingRequest

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,12 +1,22 @@
 package ru.practicum.shareit.booking;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.service.BookingService;
 
 /**
  * TODO Sprint add-bookings.
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/bookings")
 public class BookingController {
+    private final BookingService bookingService;
+
+    @PostMapping
+    public BookingDto addBookingRequest(@RequestHeader("X-Sharer-User-Id") long userId,
+                                        @RequestBody BookingDto bookingDto) {
+        return bookingService.createBookingRequest(bookingDto, userId);
+    }
 }
