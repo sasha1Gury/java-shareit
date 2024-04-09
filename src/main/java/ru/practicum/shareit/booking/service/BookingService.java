549903efd8c7c92f44ef031b2bb06dc835d2409f
sasha1Gury <sashagury456123@mail.ru>
2024-04-10 01:36:50 +0300

    feat: add new logic

--- src/main/java/ru/practicum/shareit/booking/service/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -2,6 +2,16 @@ package ru.practicum.shareit.booking.service;
 
 import ru.practicum.shareit.booking.dto.BookingDto;
 
+import java.util.List;
+
 public interface BookingService {
     BookingDto createBookingRequest(BookingDto booking, Long userId);
+
+    BookingDto updateBookingApproval(Long bookingId, boolean approved, long userId);
+
+    BookingDto getBooking(Long bookingId, long userId);
+
+    List<BookingDto> getUserBookings(String state, long userId);
+
+    List<BookingDto> getOwnerBookings(String state, long userId);
 }
