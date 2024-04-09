be46921899f867a928c021becd079c65cd7bea9c
sasha1Gury <sashagury456123@mail.ru>
2024-04-09 12:38:29 +0300

    feat: add javax persistence annotations

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -2,22 +2,34 @@ package ru.practicum.shareit.item.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.item.validation.CreateItemValidation;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 /**
  * TODO Sprint add-controllers.
  */
 @Data
+@Entity
+@Table(name = "items")
 @AllArgsConstructor
+@NoArgsConstructor
 public class Item {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     @NotBlank(groups = CreateItemValidation.class)
     private String name;
     @NotBlank(groups = CreateItemValidation.class)
     private String description;
     private Boolean available;
-    private long owner;
-    private String request;
+    @ManyToOne
+    @JoinColumn(name = "owner")
+    private User owner;
+    @OneToOne
+    @JoinColumn(name = "request")
+    private ItemRequest request;
 }
