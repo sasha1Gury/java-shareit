137e60135a5d203a4269b02812b3dbaad61c2a8c
sasha1Gury <sashagury456123@mail.ru>
2024-04-09 15:50:46 +0300

    refactor: itemService now hava a inMemory and inDb implementations

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -1,47 +1,77 @@
 package ru.practicum.shareit.item.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.exception.OwnerException;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.model.ItemMapper;
-import ru.practicum.shareit.item.repository.ItemRepositoryInMemory;
+import ru.practicum.shareit.item.repository.ItemRepository;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.repository.UserRepository;
 
+import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
+@Qualifier("ItemServiceImpl")
 public class ItemServiceImpl implements ItemService {
-    private final ItemRepositoryInMemory itemRepositoryInMemory;
+    private final ItemRepository itemRepository;
+    private final UserRepository userRepository;
 
     @Override
     public ItemDto createItem(ItemDto itemDto, long userId) {
         Item item = ItemMapper.toEntity(itemDto);
-        return ItemMapper.toDto(itemRepositoryInMemory.createItem(item, userId));
+        if (userRepository.findById(userId).isPresent()) {
+            item.setOwner(userRepository.findById(userId).get());
+        } else throw new NotFoundException(String.valueOf(userId));
+
+        return ItemMapper.toDto(itemRepository.save(item));
     }
 
     @Override
-    public ItemDto updateItem(ItemDto updateItemDto, long itemId, long userId) {
-        Item updateItem = ItemMapper.toEntity(updateItemDto);
-        return ItemMapper.toDto(itemRepositoryInMemory.updateItem(updateItem, itemId, userId));
+    public ItemDto updateItem(ItemDto updateItem, long itemId, long userId) {
+        User owner = userRepository.findById(userId).orElseThrow(() -> new NotFoundException(String.valueOf(userId)));
+        Item updates = ItemMapper.toEntity(updateItem);
+        updates.setOwner(owner);
+        Item item = itemRepository.findById(itemId).orElseThrow(() -> new NotFoundException(String.valueOf(itemId)));
+
+        if (!item.getOwner().equals(owner)) {
+            throw new OwnerException();
+        }
+        ItemMapper.updateEntity(updates, item);
+
+        return ItemMapper.toDto(itemRepository.save(item));
     }
 
     @Override
     public ItemDto findItemById(long itemId) {
-        return ItemMapper.toDto(itemRepositoryInMemory.findItemDtoById(itemId));
+        return ItemMapper.toDto(itemRepository.findById(itemId).orElseThrow(() -> new NotFoundException(String.valueOf(itemId))));
     }
 
     @Override
     public List<ItemDto> findItemByUserId(long userId) {
-        return itemRepositoryInMemory.findItemByUserId(userId).stream()
+        User owner = userRepository.findById(userId).orElseThrow(() -> new NotFoundException(String.valueOf(userId)));
+        return itemRepository.findAllByOwner(owner)
+                .stream()
                 .map(ItemMapper::toDto)
                 .collect(Collectors.toList());
     }
 
     @Override
     public List<ItemDto> searchItem(String text) {
-        return itemRepositoryInMemory.searchItem(text).stream()
+        if (text.isBlank()) {
+            return new ArrayList<>();
+        }
+        return itemRepository.search(text)
+                .stream()
+                .filter(Item::getAvailable)
                 .map(ItemMapper::toDto)
                 .collect(Collectors.toList());
     }
