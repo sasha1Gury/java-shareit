9bafda891552d0c5b08caaa928971bf3bd975795
sasha1Gury <sashagury456123@mail.ru>
2024-04-12 00:13:05 +0300

    feat: add Comment

---
+++ src/main/java/ru/practicum/shareit/item/Comment/model/Comment.java
@@ -0,0 +1,38 @@
+package ru.practicum.shareit.item.Comment.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "comments")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @Column(name = "author_name")
+    private String authorName;
+
+    @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime created;
+
+}
