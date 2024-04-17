59d6df52961a8508c8ff8a8e07c0947861f9d6f8
sasha1Gury <sashagury456123@mail.ru>
2024-04-17 20:43:09 +0300

    feat: add createItemRequest

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,12 +1,22 @@
 package ru.practicum.shareit.request;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.service.ItemRequestService;
 
 /**
  * TODO Sprint add-item-requests.
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/requests")
 public class ItemRequestController {
+    private final ItemRequestService itemRequestService;
+
+    @PostMapping
+    public ItemRequestDto createItemRequest(@RequestHeader("X-Sharer-User-Id") long userId,
+                                            @RequestBody ItemRequestDto itemRequestDto) {
+        return itemRequestService.createItemRequest(itemRequestDto, userId);
+    }
 }
