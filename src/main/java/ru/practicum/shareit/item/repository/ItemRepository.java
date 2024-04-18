96488396c7b9153a28935f0d4b00f173003af02d
sasha1Gury <sashagury456123@mail.ru>
2024-04-19 00:33:56 +0300

    feat: add pagination

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -1,5 +1,8 @@
 package ru.practicum.shareit.item.repository;
 
+import lombok.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.item.model.Item;
@@ -10,10 +13,18 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findAllByOwner(User user);
 
+    @NonNull
+    Page<Item> findAllByOwner(@NonNull Pageable pageable, User owner);
+
     @Query(" select i from Item i " +
             "where upper(i.name) like upper(concat('%', ?1, '%')) " +
             " or upper(i.description) like upper(concat('%', ?1, '%'))")
     List<Item> search(String text);
+
+    @Query(" select i from Item i " +
+            "where upper(i.name) like upper(concat('%', ?1, '%')) " +
+            " or upper(i.description) like upper(concat('%', ?1, '%'))")
+    Page<Item> search(@NonNull Pageable pageable, String text);
 
     List<Item> findAllByRequest_Id(long requestId);
 
