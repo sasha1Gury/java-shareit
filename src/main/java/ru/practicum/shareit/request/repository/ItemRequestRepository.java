90c8b7a8678636c50a2de1a78e341d3fee5106fb
sasha1Gury <sashagury456123@mail.ru>
2024-04-18 12:53:51 +0300

    feat: add getAll and get by id

--- src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
+++ src/main/java/ru/practicum/shareit/request/repository/ItemRequestRepository.java
@@ -1,7 +1,23 @@
 package ru.practicum.shareit.request.repository;
 
+import lombok.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
+    List<ItemRequest> findAllByOwnerOrderByCreatedDesc(User owner);
+
+    Optional<ItemRequest> findById(long id);
+
+    @NonNull
+    Page<ItemRequest> findAll(@NonNull Pageable pageable);
+
+    @NonNull
+    List<ItemRequest> findAll();
 }
