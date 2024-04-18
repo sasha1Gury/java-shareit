9280d281521949972cee97bb16caa9b5a3234cea
sasha1Gury <sashagury456123@mail.ru>
2024-04-18 12:51:25 +0300

    feat: add request now long requestId

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.item.validation.CreateItemValidation;
-import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
 import javax.validation.constraints.NotBlank;
@@ -23,5 +22,5 @@ public class ItemDto {
     @NotNull(groups = CreateItemValidation.class)
     private Boolean available;
     private User owner;
-    private ItemRequest request;
+    private Long requestId;
 }
