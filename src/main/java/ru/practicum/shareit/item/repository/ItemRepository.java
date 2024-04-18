a5a9b6f1fc94b1b1638697e2db817afb2506b89b
sasha1Gury <sashagury456123@mail.ru>
2024-04-18 12:52:32 +0300

    feat: add find all by requestId

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -15,4 +15,6 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
             " or upper(i.description) like upper(concat('%', ?1, '%'))")
     List<Item> search(String text);
 
+    List<Item> findAllByRequest_Id(long requestId);
+
 }
