f51d1577ca469dcae7b3228b3fdfbfd68ab00851
sasha1Gury <sashagury456123@mail.ru>
2024-04-18 12:53:24 +0300

    feat: add getAll and get by id

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.service.ItemRequestService;
 
+import java.util.List;
+
 /**
  * TODO Sprint add-item-requests.
  */
@@ -18,5 +20,23 @@ public class ItemRequestController {
     public ItemRequestDto createItemRequest(@RequestHeader("X-Sharer-User-Id") long userId,
                                             @RequestBody ItemRequestDto itemRequestDto) {
         return itemRequestService.createItemRequest(itemRequestDto, userId);
+    }
+
+    @GetMapping
+    public List<ItemRequestDto> getAllItemRequests(@RequestHeader("X-Sharer-User-Id") long userId) {
+        return itemRequestService.getAllItemRequestsByOwner(userId);
+    }
+
+    @GetMapping("/all")
+    public List<ItemRequestDto> getAllRequests(@RequestHeader("X-Sharer-User-Id") long userId,
+                                               @RequestParam(required = false) Integer from,
+                                               @RequestParam(required = false) Integer size) {
+        return itemRequestService.getAllRequests(from, size, userId);
+    }
+
+    @GetMapping("/{requestId}")
+    public ItemRequestDto getById(@RequestHeader("X-Sharer-User-Id") long userId,
+                                  @PathVariable("requestId") long requestId) {
+        return itemRequestService.getById(requestId, userId);
     }
 }
