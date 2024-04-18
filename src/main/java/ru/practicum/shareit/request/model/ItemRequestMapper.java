784f31f9b14493b5ff14e6bfff9aa19ed5ee529f
sasha1Gury <sashagury456123@mail.ru>
2024-04-18 12:54:54 +0300

    feat: fix for new fields

--- src/main/java/ru/practicum/shareit/request/model/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequestMapper.java
@@ -1,17 +1,22 @@
 package ru.practicum.shareit.request.model;
 
 import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.ArrayList;
 
 public class ItemRequestMapper {
     public static ItemRequest toEntity(ItemRequestDto dto) {
         return new ItemRequest(dto.getId(),
                 dto.getDescription(),
-                dto.getOwner());
+                new User(),
+                dto.getCreated());
     }
 
     public static ItemRequestDto toDto(ItemRequest itemRequest) {
         return new ItemRequestDto(itemRequest.getId(),
                 itemRequest.getDescription(),
-                itemRequest.getOwner());
+                itemRequest.getCreated(),
+                new ArrayList<>());
     }
 }
