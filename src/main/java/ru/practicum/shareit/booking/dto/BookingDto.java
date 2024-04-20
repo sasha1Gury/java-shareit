cffc62a85d737d72bf85ef4d24f59dc68067b0ce
sasha1Gury <sashagury456123@mail.ru>
2024-04-20 19:45:29 +0300

    feat: add NoArgsConstructor

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.booking.model.Status;
 import ru.practicum.shareit.booking.validation.CreateBookingValidation;
 import ru.practicum.shareit.item.model.Item;
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookingDto {
     private long id;
     @FutureOrPresent(groups = CreateBookingValidation.class)
