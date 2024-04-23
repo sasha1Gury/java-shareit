3f6e11bb21f91e4cf07b07f907a195bc50e6e3be
sasha1Gury <sashagury456123@mail.ru>
2024-04-23 14:50:11 +0300

    style: UserServiceImplTest

--- src/test/java/ru/practicum/shareit/unit/UserServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/unit/UserServiceImplTest.java
@@ -4,43 +4,56 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.model.UserMapper;
 import ru.practicum.shareit.user.repository.UserRepository;
-import ru.practicum.shareit.user.service.UserService;
 import ru.practicum.shareit.user.service.UserServiceImpl;
 
 import java.util.Optional;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class UserServiceImplTest {
+
     @Mock
     private UserRepository userRepository;
+
     @InjectMocks
     private UserServiceImpl userService;
 
+    private long userId = 1;
+
     @Test
     public void updateUserTest() {
+        // Set up a mock user
         User user = new User();
-        user.setId(1);
+        user.setId(userId);
         user.setEmail("testEmail");
         user.setName("name");
 
+        // Set up an updated user
         User updateUser = new User();
-        updateUser.setId(1);
+        updateUser.setId(userId);
         updateUser.setName("updateName");
         updateUser.setEmail("");
 
+        // Update the user entity
+        UserMapper.updateEntity(user, updateUser);
+
+        // Set up mock repository responses
         when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
+        when(userRepository.save(any())).thenReturn(updateUser);
 
-        UserDto result = userService.updateUser(UserMapper.toDto(updateUser), user.getId());
+        // Call the method under test
+        UserDto result = userService.updateUser(UserMapper.toDto(updateUser), userId);
 
+        // Assertions
         assertEquals(updateUser.getName(), result.getName());
     }
 }
