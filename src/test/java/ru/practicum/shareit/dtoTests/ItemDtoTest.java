732797f3d638870ec4b9f32eb7831a8bc245061c
sasha1Gury <sashagury456123@mail.ru>
2024-05-01 16:43:05 +0300

    feat: add ItemDtoTest

---
+++ src/test/java/ru/practicum/shareit/dtoTests/ItemDtoTest.java
@@ -0,0 +1,29 @@
+package ru.practicum.shareit.dtoTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import ru.practicum.shareit.item.dto.ItemDto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class ItemDtoTest {
+
+    @Autowired
+    private JacksonTester<ItemDto> json;
+
+    @Test
+    void testItemDto() throws Exception {
+        ItemDto itemDto = ItemDto.builder().id(1L).name("вещь").description("описание вещи").available(true).build();
+
+        JsonContent<ItemDto> result = json.write(itemDto);
+
+        assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(1);
+        assertThat(result).extractingJsonPathStringValue("$.name").isEqualTo("вещь");
+        assertThat(result).extractingJsonPathStringValue("$.description").isEqualTo("описание вещи");
+        assertThat(result).extractingJsonPathBooleanValue("$.available").isEqualTo(true);
+    }
+}
